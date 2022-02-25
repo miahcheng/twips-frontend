e7710236862dee8d4ef5486a9f9a944ebdc91608
chengj29 <chengj29@uw.edu>
2022-02-24 21:47:11 -0800

    starting example page

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import Box from '@mui/material/Box';
+import { Typography } from "@mui/material";
+import { createStyles, makeStyles } from '@mui/styles';
+import { createTheme, Theme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        HomePage: {
+            justifyContent: 'center',
+        },
+        title: {
+            flexGrow: 1,
+        },
+        drawer: {
+            width: 300,
+        },
+        fullList: {
+            width: 'auto',
+        },
+    }),
+);
 const Home = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1> This is the home page</h1>
-        </div>
+        <Grid container spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}>
+            <Grid item xs={12}>
+                <Box bgcolor="purple" color="white" display="flex" justifyContent="center" alignItems="center">
+                    <Typography>
+                        Welcome to Twips
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
